74e7f2835a156f5ff042a7db76086d29f3e3b807
Hossam Amer <hossam.amer666@gmail.com>
2022-06-19 14:58:32 +0200

    first commit

---
+++ HelloNavisApi/Main.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Navisworks.Api.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNavisApi
+{
+  public class Main : CommandHandlerPlugin
+  {
+    [Plugin("Hello Navis", "Diroots", DisplayName = "Hello Navis")]
+    public override int ExecuteCommand(string name, params string[] parameters)
+    {
+      return 0;
+    }
+  }
+}
