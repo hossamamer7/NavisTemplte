f8e64749b871f53c2e157f69fda70c603ac47c5a
Hossam Amer <hossam.amer666@gmail.com>
2022-06-19 16:10:44 +0200

    feat: add images to command button

--- HelloNavisApi/Main.cs
+++ HelloNavisApi/Main.cs
@@ -1,17 +1,22 @@
 ﻿using Autodesk.Navisworks.Api.Plugins;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace HelloNavisApi
 {
+  [Plugin("Hello Navis", "Diroots", DisplayName = "Hello Navis")]
+  [RibbonLayout("HelloNavis.xaml")]
+  [RibbonTab("ID_HelloNavis_1")]
+  [Command("ID_Button_1",Icon = "icon16.png",LargeIcon = "icon32.png", ToolTip = "Show a message", DisplayName ="Say Hello")]
   public class Main : CommandHandlerPlugin
   {
-    [Plugin("Hello Navis", "Diroots", DisplayName = "Hello Navis")]
     public override int ExecuteCommand(string name, params string[] parameters)
     {
+      switch (name)
+      {
+        case "ID_Button_1":
+          MessageBox.Show("Hello Navis!");
+          break;
+      }
       return 0;
     }
   }
